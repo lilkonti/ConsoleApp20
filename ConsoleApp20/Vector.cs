bcbfd01c777609236d16f18ea3b29c8df0adb96c
lilkonti <lilkonti272@gmail.com>
2023-05-29 00:18:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp20/Vector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Vector3D
+    {
+        public int x; public int y; public int z;
+        public void mult(int x, int y, int z)
+        {
+            x *= this.x;
+            y *= this.y;
+            z *= this.z;
+        }
+        public void plus(int x, int y, int z)
+        {
+            x += this.x;
+            y += this.y;
+            z += this.z;
+        }
+        public void minus(int x, int y, int z)
+        {
+            x -= this.x;
+            y -= this.y;
+            z -= this.z;
+        }
+    }
+}
---
+++ ConsoleApp20/numbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal struct Numbers
+    {
+        int originalNum;
+        public Numbers()
+        {
+            originalNum = 8;
+        }
+        void toBinary()
+        {
+            int from = 10;
+            int to = 2;
+
+            string binary = Convert.ToString(Convert.ToInt32(originalNum, from), to);
+            Console.WriteLine(binary);
+        }
+        void toEight() 
+        {
+            int from = 10;
+            int to = 8;
+
+            string binary = Convert.ToString(Convert.ToInt32(originalNum, from), to);
+            Console.WriteLine(binary);
+        }
+        void toSixteen()
+        {
+            int from = 10;
+            int to = 16;
+
+            string binary = Convert.ToString(Convert.ToInt32(originalNum, from), to);
+            Console.WriteLine(binary);
+        }
+    }
+}
